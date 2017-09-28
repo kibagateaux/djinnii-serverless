c1085ee263f966076c9574d1969e59150a92f89f
Kiba Gateaux <kibagateaux@gmail.com>
2017-09-28 15:13:32 -0400

    initial commit

---
+++ project.clj
@@ -0,0 +1,30 @@
+(defproject djinnii-serverless "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure       "1.8.0"]
+                 [org.clojure/clojurescript "1.8.51"]
+                 [io.nervous/cljs-lambda    "0.3.5"]
+                 [adzerk/env "0.4.0"]]
+  :plugins [[lein-npm                    "0.6.2"]
+            [io.nervous/lein-cljs-lambda "0.6.6"]]
+  :npm {:dependencies [[serverless-cljs-plugin "0.1.2"]
+                        [serverless-offline "3.16.0"]]}
+  :cljs-lambda {:compiler
+                {:inputs  ["src"]
+                 :options {:output-to     "target/djinnii-serverless/djinnii_serverless.js"
+                           :output-dir    "target/djinnii-serverless"
+                           :target        :nodejs
+                           :language-in   :ecmascript5
+                           :optimizations :advanced}}})
+  :cljsbuild {
+    :repl-listen-port 9000
+    :builds {
+      :dev
+      {:source-paths ["src"]
+       :jar true
+       :compiler {:output-to "resources/public/js/main-debug.js"
+                  :optimizations :whitespace
+                  :pretty-print true}}
+      :prod
+      {:source-paths ["src"]
+       :compiler {:output-to "resources/public/js/main.js"
+                  :optimizations :advanced
+                  :pretty-print false}}}}
---
+++ src/djinnii_serverless/moves-api/auth.cljs
@@ -0,0 +1,26 @@
+(ns djinnii-serverless.moves-api.auth
+  (:require [cljs-lambda.macros :refer-macros [deflambda]])
+  (:require-macros [adzerk.env :as env]))
+
+(def log (.-log js/console))
+(def env-var (.-env js/process))
+(deflambda init-auth
+  [event context]
+  ;; switch case on invocation method get/post
+  ;; on post and code respond back to moves api
+  ;; auth code for token url https://api.moves-app.com/oauth/v1/access_token?grant_type=authorization_code&code=<code>&client_id=<client_id>&client_secret=<client_secret>&redirect_uri=<redirect_uri>
+
+  ;; if get then initiate process
+  (let [api-key (env/def "MOVES_API_KEY")
+        redirect-uri (str "moves://app/authorize?client_id=" api-key "&redirect_uri=" "&scope=activity,location")]
+    (log "key" api-key)
+    (log "url" redirect-uri)
+    (log "context" context)
+    (log "event" event)
+    ;; return deep-link
+    
+    (assoc-in (js->clj event) [:response] redirect-uri)
+    (.done context nil event)
+  )
+  ;; on get request initiates auth flow with 
+)
