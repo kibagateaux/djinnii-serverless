79220bf9de3362265731d002fd2e1169af671c03
Kiba Gateaux <kibagateaux@gmail.com>
2017-11-18 09:29:17 -0500

    renames variables and adds notes

--- integrations/srcES7/services/movesSvc/index.js
+++ integrations/srcES7/services/movesSvc/index.js
@@ -1,5 +1,5 @@
 import Moves from 'moves';
-import _ from 'lodash';
+import{toUpper} from 'lodash';
 import {
   createActivitiesList,
   createDailyLedger,
@@ -12,7 +12,7 @@ export const getMovesStorylineData = (event, context) => {
   const userId = event.pathParameters.userId;
   if(userId) {
     const queryParams = {
-      TableName: process.env.DYNAMODB_TOKENS_TABLE || "djinii-mobilehub-1897344653-Tokens",
+      TableName: process.env.DYNAMODB_TOKENS_TABLE,
       Key: {userId}
     };
     DB.get(queryParams, (error, results) => {
@@ -22,7 +22,7 @@ export const getMovesStorylineData = (event, context) => {
       } else if (results.Item && results.Item.moves) {   // if has tokens get data 
         const {access_token, refresh_token} = results.Item.moves;
         const moves = new Moves({
-          client_id: process.env.MOVES_API_KEY,
+          client_id: process.env.MOVES_API_KEY, // manually added in AWS console not in .yaml file
           access_token,
           refresh_token
         });
@@ -40,16 +40,16 @@ export const getMovesStorylineData = (event, context) => {
               , {}); // compiles full list of new data from each day to update to DB
 
             const dbWrites = Object.keys(newData).map((resource) => {
-              const data = newData[resource]
+              const data = newData[resource];
               const ledger = Object.keys(data).map((time) => data[time]);
               const blobs = blobify(ledger);
-              const table = process.env[`DYNAMODB_${_.toUpper(resource)}_TABLE`];
-              return (blobs[0][0].length > 0) ? // checks that there is at least one item to put
+              const table = process.env[`DYNAMODB_${toUpper(resource)}_TABLE`];
+              return (blobs[0].length > 0) ? // checks that there is at least one item to put
                 blobs.map((blob) => batchPut(table, blob, userId)) : null;
             });
 
             // don't think using results from writes is useful but this is how to handle it
-            // .reduce((writes, put) => [...writes, ...put], []);
+            // .reduce((ledger, writes) => [...ledger, ...writes], []);
             // Promise.all(dbWrites)
             //   .then((result) => console.log('dbwrite res', result))
             //   .catch((error) => console.log('write err', error))
