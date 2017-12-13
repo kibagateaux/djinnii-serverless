dc837c6312a57595f5dbe518871abe5f11277d5f
Kiba Gateaux <kibagateaux@gmail.com>
2017-12-13 12:45:24 -0500

    replaces express + graphql with apollo-lambda package

--- graphql/srcES7/handler.js
+++ graphql/srcES7/handler.js
@@ -1,5 +1,26 @@
-import awsServerlessExpress from 'aws-serverless-express';
-import server from './server';
- 
-export const GraphQLServer = (event, context) => 
-  awsServerlessExpress.proxy(server, event, context);
+import {
+  graphqlLambda,
+  graphiqlLambda
+} from 'apollo-server-lambda';
+import {v1 as neo4j} from 'neo4j-driver';
+import schema from './schema';
+
+console.log('server', graphiqlLambda);
+
+function graphContext(headers, secrets, event, ctx) {
+  const driver = neo4j.driver(
+    secrets.NEO4J_URI || "bolt://localhost:7687",
+    neo4j.auth.basic(secrets.NEO4J_USER || "neo4j",
+    secrets.NEO4J_PASSWORD || "kiba")
+  );
+  return {driver, headers, lambdaEvent: event, lambdaContext: ctx};
+};
+
+export const GraphQLServerHandler = graphqlLambda((event, ctx) => ({ 
+  schema,
+  context: graphContext(event.headers, process.env, event, ctx)
+}));
+
+export const GraphQLInspectorHandler = graphiqlLambda({
+  endpointURL: '/'
+});
