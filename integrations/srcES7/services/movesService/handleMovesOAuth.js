64d5348cc686d53d5814c4c9c9ca7151fb9f7876
Kiba Gateaux <kibagateaux@gmail.com>
2018-01-08 12:48:43 -0500

    removes axios import for moves OAuth handler

--- integrations/srcES7/services/movesService/handleMovesOAuth.js
+++ integrations/srcES7/services/movesService/handleMovesOAuth.js
@@ -1,6 +1,6 @@
 import AWS from 'aws-sdk';
 import Moves from 'react-native-moves-api';
-import axios from 'axios';
+
 const lambda = new AWS.Lambda({apiVersion: '2015-03-31'});
 const moves = new Moves({
   client_secret: process.env.MOVES_CLIENT_SECRET,
@@ -17,15 +17,15 @@ export const handleMovesOAuth = (event, context, callback) => {
       const {access_token, refresh_token} = res.data;
       const stateParams = state.split(' ');
       const [redirectId, userId] = stateParams.map((param) => param.split('=')[1]);
-      const data = {
-        userId,
-        redirectId,
-        integrationName: "moves",
-        accessToken: access_token,
-        refreshToken: refresh_token
-      };
-
       if(userId.length && redirectId.length) {
+        const data = {
+          userId,
+          redirectId,
+          integrationName: "Moves",
+          accessToken: access_token,
+          refreshToken: refresh_token
+        };
+
         var params = {
           FunctionName: "jinni-integrations-dev-updateOAuthTokens", 
           InvocationType: "Event", 
@@ -36,31 +36,17 @@ export const handleMovesOAuth = (event, context, callback) => {
           if(!error) {
             console.log('handle moves success', data);
             const response = {
-              isBase64Encoded: false,
-              statusCode: 303,
-              headers: {
-                "location": "https://emochi.app.link/moves/init-auth",
-                "Content-Type": "application/json",
-                "Access-Control-Request-Method": "GET",
-                "Access-Control-Allow-Origin": "*"
-              }, // FIXME: get deeplinks to work
-              data: JSON.stringify({})
+              statusCode: 301,
+              headers: {Location: "https://emochi.app.link"}, // FIXME: get deeplinks to work
+              data: ""
             };
+            console.log('handle redirect', response);
             callback(null, response)
           } else {
-            callback(error, null)
             console.log('handle moves error', error);
+            callback(error, null)
           }
         });
-
-        // For dev testing
-        // axios.post("http://localhost:3000/updateTokens", data)
-        //   .then((result) => {
-        //     console.log('Moves done updating tokens');
-        //   })
-        //   .catch((error) => {
-        //     console.log('Moves error updating tokens', error);
-        //   });
       }
     })
     .catch((err) => {
@@ -68,7 +54,7 @@ export const handleMovesOAuth = (event, context, callback) => {
       callback(err);
     });
  } else {
-   const noCodeError = new Error("No authorization code supplied in query");
+   const noCodeError = new Error("No authorization code or state supplied in query");
    callback(noCodeError, params)
    // init auth process
    // TODO: Fix rn-moves-api moves.authorize to not require exporess
