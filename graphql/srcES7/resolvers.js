20ee4939072fd872f4dad2a36c3a148652f49a3b
Kiba Gateaux <kibagateaux@gmail.com>
2018-01-30 13:49:12 -0500

    adds automation for queries

--- graphql/srcES7/resolvers.js
+++ graphql/srcES7/resolvers.js
@@ -1,11 +1,21 @@
 import {neo4jgraphql} from 'neo4j-graphql-js';
+import querySchema from './type-definitions/Query';
 
-// How to automate this?
-// for Queries
-// import Query from './type-definitions/Query'
-// queryName = /^(\s*):.*/.exec(Query)
-// factoryfunc(queryName)
-// Query {...factoriedFunctions}
+const queries = querySchema.split('\n');
+const queryNames = queries
+  .map((q) => /([A-Za-z]+)(?=[:|(])[\s\S]*/mg.exec(q), []) // gets queryname from beginning of line
+  .filter(is => is) // filters null matches
+  .map((match) => match[1]); // parses oout just match string
+
+/**
+ * @desc takes list of query names and creates Query object with Neo4j integration 
+ */
+const Query = queryNames.reduce((qs, q) =>({
+  ...qs,
+  [q]: (obj, args, cts, resolveInfo) =>
+    neo4jgraphql(obj, args, cts, resolveInfo)
+}), {})
+
 
 // for Mutations
 // import Mutations from ./mutations
@@ -16,15 +26,5 @@ import {neo4jgraphql} from 'neo4j-graphql-js';
 // Mutation {...factoriedFuncs}
 
 export default {
-  Query: {
-    User(object, params, ctx, resolveInfo) {
-      return neo4jgraphql(object, params, ctx, resolveInfo);
-    },
-    AllUsers(object, params, ctx, resolveInfo) {
-      return neo4jgraphql(object, params, ctx, resolveInfo);
-    },
-    UserActivities(object, params, ctx, resolveInfo) {
-      return neo4jgraphql(object, params, ctx, resolveInfo);
-    }
-  }
+  Query
 };
